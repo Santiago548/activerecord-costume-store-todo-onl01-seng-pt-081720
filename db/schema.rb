661cd1927d0070fd5efee7eaa96358bfdba2383b
Dominic Barajas <dominicbarajas548@gmail.com>
2020-10-28 17:59:16 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -23,7 +23,7 @@ ActiveRecord::Schema.define(version: 3) do
  t.datetime "created_at", null: false
  t.datetime "updated_at", null: false
 end
-    
+
 
   create_table "costume_stores", force: :cascade do |t|
     t.string "name"
