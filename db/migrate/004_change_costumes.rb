f48fdf7406bd0fc1db53c53ce648569740208a97
Dominic Barajas <dominicbarajas548@gmail.com>
2020-10-28 17:37:29 +0000

    Automatically backed up by Learn

--- db/migrate/004_change_costumes.rb
+++
@@ -1,8 +0,0 @@
-class ChangeCostumes < ActiveRecord::Migration[5.1]
-  def change
-    change_column :costumes, :name, :string
-    change_column :costumes, :price, :float
-    change_column :costumes, :image_url, :string
-    change_column :costumes, :size, :string
-  end
-end
