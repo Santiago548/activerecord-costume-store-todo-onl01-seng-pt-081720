c4adfbe32d7faa73fdc17281acea50e56b8e168a
Dominic Barajas <dominicbarajas548@gmail.com>
2020-10-28 18:00:34 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -25,13 +25,13 @@ ActiveRecord::Schema.define(version: 3) do
   end
 
   create_table "costumes", force: :cascade do |t|
-  t.string "name"
-  t.string "size"
-  t.string "image_url"
-  t.float "price"
-  t.datetime "created_at", null: false
-  t.datetime "updated_at", null: false
-end
+    t.string "name"
+    t.string "size"
+    t.string "image_url"
+    t.float "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "haunted_houses", force: :cascade do |t|
     t.string "name"
