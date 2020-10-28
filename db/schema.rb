5f74d0289af82055b8e6fe544b846fced5708493
Dominic Barajas <dominicbarajas548@gmail.com>
2020-10-28 18:00:18 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -13,19 +13,6 @@
 ActiveRecord::Schema.define(version: 3) do
 
   create_table "costume_stores", force: :cascade do |t|
- t.string "name"
- t.string "location"
- t.integer "costume_inventory"
- t.integer "num_of_employees"
- t.boolean "still_in_business"
- t.datetime "opening_time"
- t.datetime "closing_time"
- t.datetime "created_at", null: false
- t.datetime "updated_at", null: false
-end
-
-
-  create_table "costume_stores", force: :cascade do |t|
     t.string "name"
     t.string "location"
     t.integer "costume_inventory"
@@ -36,6 +23,15 @@ end
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "costumes", force: :cascade do |t|
+  t.string "name"
+  t.string "size"
+  t.string "image_url"
+  t.float "price"
+  t.datetime "created_at", null: false
+  t.datetime "updated_at", null: false
+end
 
   create_table "haunted_houses", force: :cascade do |t|
     t.string "name"
